d3f0bc98f9bbbf7cddc522883fe1d9d766ba0a3e
htotapally <Sharma@IF-SWS-HHXJ5L2.ad.ufl.edu>
2018-07-11 20:04:12 -0400

    Corrected directory structure

---
+++ src/main/java/com/acme/learning/util/Test.java
@@ -0,0 +1,10 @@
+package com.acme.learning.util;
+
+public class Test {
+
+	private static final String VERSION = "0.0.1";
+	
+	public static String getVersion() {
+		return VERSION;
+	}
+}
---
+++ src/test/java/com/acme/learning/util/TestTest.java
@@ -0,0 +1,14 @@
+package com.acme.learning.util;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestTest {
+
+	@Test
+	public void testGetVersion() {
+		fail("Not yet implemented");
+	}
+
+}
